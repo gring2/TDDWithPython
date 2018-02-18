43eccb6f8b867f7f9bda366426cb73c5359e78ff
gring2 <pjw6454@hotmail.com>
2018-02-18 20:52:37 +0900

    Redirect after Post, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -31,10 +31,18 @@ class NewVisitorTest(unittest.TestCase) :
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box(Edith's hobby is typing fly-fishing lures)
-        inputbox.send_keys('Use peacock peacock feathers to make a fly')
+#        inputbox.send_keys('Use peacock peacock feathers to make a fly')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1 : Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
--- lists/tests.py
+++ lists/tests.py
@@ -30,9 +30,15 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(resp, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        resp = self.client.post('/', data={'item_text' : 'A new list item'})
-        self.assertIn('A new list item', resp.content.decode())
-        self.assertTemplateUsed(resp, 'home.html')
+        resp = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        resp = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp['location'], '/')
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -50,3 +56,15 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        resp = self.client.get('/')
+        self.assertIn('itemey 1', resp.content.decode())
+        self.assertIn('itemey 2', resp.content.decode())
