2ded930200e9d86ad8bbdc0660210dda0872a01b
gring2 <pjw6454@hotmail.com>
2018-02-22 21:34:20 +0900

    make new function

--- lists/tests.py
+++ lists/tests.py
@@ -21,7 +21,7 @@ class HomePageTest(TestCase):
         resp = self.client.get('/')
         html = resp.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertIn('<title>Start a new To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(resp, 'home.html')
 
@@ -30,21 +30,37 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(resp, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        resp = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        resp = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(resp.status_code, 302)
-        self.assertEqual(resp['location'], '/')
+        resp = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(resp['location'], '/lists/the-only-list-in-the-world/')
 
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        resp = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertContains(resp, 'itemey 1')
+        self.assertContains(resp, 'itemey 2')
+
+    def test_uses_list_template(self):
+        resp = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(resp, 'list.html')
+
+
+class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
-        
+
         second_item = Item()
         second_item.text = 'Item the second'
         second_item.save()
@@ -57,14 +73,3 @@ class HomePageTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        resp = self.client.get('/')
-        self.assertIn('itemey 1', resp.content.decode())
-        self.assertIn('itemey 2', resp.content.decode())
